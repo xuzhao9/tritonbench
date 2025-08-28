3b90c4f7ba6d1dacfdcdf15349d7dec59cb9bd74
Yueming Hao <yhao@meta.com>
2025-08-27 20:39:54 -0700

    remove warmup

--- tritonbench/components/do_bench/run.py
+++ tritonbench/components/do_bench/run.py
@@ -154,18 +154,19 @@ def do_bench_nrep(fn, warmup=25, rep=100, grad_to_none=None, quantiles=None, ret
     cache = runtime.driver.active.get_empty_cache_for_benchmark()
 
     # Estimate the runtime of the function
-    start_event = di.Event(enable_timing=True)
-    end_event = di.Event(enable_timing=True)
-    start_event.record()
-    for _ in range(5):
-        runtime.driver.active.clear_cache(cache)
-        fn()
-    end_event.record()
+    # start_event = di.Event(enable_timing=True)
+    # end_event = di.Event(enable_timing=True)
+    # start_event.record()
+    # for _ in range(5):
+    #     runtime.driver.active.clear_cache(cache)
+    #     fn()
+    # end_event.record()
     di.synchronize()
-    estimate_ms = start_event.elapsed_time(end_event) / 5
+    # estimate_ms = start_event.elapsed_time(end_event) / 5
 
     # compute number of warmup and repeat
-    n_warmup = max(1, int(warmup / estimate_ms))
+    # n_warmup = max(1, int(warmup / estimate_ms))
+    n_warmup = 2
     n_repeat = max(1, int(rep))
     start_event = [di.Event(enable_timing=True) for i in range(n_repeat)]
     end_event = [di.Event(enable_timing=True) for i in range(n_repeat)]
